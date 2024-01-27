153536e85a25b61969f392de42ce4f815b7840cd
ac618 <andychou618@gmail.com>
2024-01-27 18:54:54 +0800

    Player basics complete

--- Assets/Scripts/CharacterControl/PlayerMovement.cs
+++ Assets/Scripts/CharacterControl/PlayerMovement.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Texture2D cursorTexture; // Assign your cursor image in the Unity Editor
+    private float speed = 0.7f;
 
-    [SerializeField] [Range(0.3f, 1f)]
-    private float speed = 0.5f;
+    [SerializeField] [Range(0.4f, 1.2f)]
+    private float walkSpeed = 0.7f;
+
+    [SerializeField] [Range(0.5f, 2f)]
+    private float runSpeed = 1.4f;
 
     [SerializeField] [Range(200f, 600f)]
     private float rotateSensitivity = 400f;
@@ -34,6 +38,8 @@ public class PlayerMovement : MonoBehaviour
         //calculate movement
         moveDirection.Set(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         moveDirection.Normalize();
+        // run
+        speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
         // rotate character
         transform.Rotate(Input.GetAxis("Mouse X") * Vector3.up * Time.deltaTime * rotateSensitivity);
         verticalRoatation += Input.GetAxis("Mouse Y") * Time.deltaTime * rotateSensitivity;
@@ -53,8 +59,8 @@ public class PlayerMovement : MonoBehaviour
     {
         // Get the center of the screen
         Vector2 centerScreen = new Vector2(Screen.width / 2, Screen.height / 2);
-
+        float scale = 0.08f;
         // Draw the cursor at the center of the screen
-        GUI.DrawTexture(new Rect(centerScreen.x - (cursorTexture.width / 2), centerScreen.y - (cursorTexture.height / 2), cursorTexture.width, cursorTexture.height), cursorTexture);
+        GUI.DrawTexture(new Rect(centerScreen.x - (scale * cursorTexture.width / 2), centerScreen.y - (scale * cursorTexture.height / 2), scale * cursorTexture.width, scale * cursorTexture.height), cursorTexture);
     }
 }
